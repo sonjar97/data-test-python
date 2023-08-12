bd6f07ddfb3b6998ea8ff02f96e96d87676f1a28
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-08-12 13:38:44 +0100

    Update data_join.py

--- exercises/pyspark/03_data_join/data_join.py
+++ exercises/pyspark/03_data_join/data_join.py
@@ -10,5 +10,44 @@ The output of your work should be a JSON file, output.json.
 """
 
 
-def data_join():
-    pass
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, StringType
+from pyspark.sql.functions import col, avg, sum, count
+
+
+class JoinData:
+
+    users_schema = StructType([
+        StructField('user_id', IntegerType(), False),
+        StructField('name', StringType(), True)
+    ])
+    purchases_schema = StructType([
+        StructField('user_id', IntegerType(), False),
+        StructField('price', FloatType(), False)
+    ])
+
+    def __init__(self, file_path_users, file_path_purchases):
+        self.spark = SparkSession.builder.appName('JoinData').getOrCreate()
+
+        self.df_users = self.spark.read.csv(file_path_users, header=True).dropDuplicates(['user_id'])
+        self.df_purchases = self.spark.read.csv(file_path_purchases, header=True)
+
+
+    def data_join(self):
+        self.df_join = self.df_users.join(
+            self.df_purchases,
+            how='inner',
+            on=['user_id']
+            ).select(self.df_users['user_id'], self.df_users['name'], self.df_purchases['price'])
+
+
+    def data_join_aggregation(self):
+        self.data_join()
+
+        self.df_join = self.df_join.groupBy('user_id', 'name').agg(
+            sum('price').alias('total_spending'),
+            count('user_id').alias('purchase_count'),
+            avg('price').alias('average_spending')
+        )
+
+        self.df_join.write.mode('overwrite').json('output.json')
