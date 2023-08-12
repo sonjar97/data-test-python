3dc2f76fc32534d2f3244cec7a8a02fde0913a8d
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-08-12 13:29:49 +0100

    Update data_aggregation.py

--- exercises/pyspark/02_data_aggregation/data_aggregation.py
+++ exercises/pyspark/02_data_aggregation/data_aggregation.py
@@ -11,5 +11,49 @@ The output should be sorted in descending order based on the total number of eve
 """
 
 
-def data_aggregation():
-    pass
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, DateType
+from pyspark.sql.functions import col, sum, expr
+
+
+class AggregateData:
+
+    def __init__(self, file_path_or_data=None, columns=None):
+
+        self.spark = SparkSession.builder.appName("AggregateData").getOrCreate()
+
+        if file_path_or_data is not None and columns is None:
+            self.df = self.spark.read.option('multiline','true').json(file_path_or_data) \
+                                .select('event_date', 'event_count')
+        elif file_path_or_data is not None and columns is not None:
+            self.df = self.spark.read.json(
+                self.spark.sparkContext.parallelize(file_path_or_data)
+            ).select('event_date', 'event_count')
+
+
+    def data_cleaning(self):
+        self.df = self.df \
+            .filter(
+                col('event_date').cast(DateType()).isNotNull() &
+                col('event_count').cast(IntegerType()).isNotNull()) \
+            .filter(
+                expr('event_count = floor(event_count)')
+            )
+        
+        self.df = self.df.withColumn('event_date', col('event_date').cast(DateType())) \
+                         .withColumn('event_count', col('event_count').cast(IntegerType()))
+        
+        self.df = self.df.filter(col('event_count') > 0)
+
+
+    def calculate_total_events_per_day(self):
+        self.df = self.df.groupBy('event_date') \
+                         .agg(sum('event_count').alias('total_events'))
+
+
+    def data_aggregation(self):
+        self.data_cleaning()
+        self.calculate_total_events_per_day()
+
+        self.df.orderBy(col('total_events').desc()) \
+               .write.mode('overwrite').csv('output.csv')
